1f78b51720ccab207a29121f79c35efd8f1c65ea
Khushi-Dixit <146416563+Khushi-Dixit@users.noreply.github.com>
2024-08-30 11:22:41 +0530

    Update q_3.py

--- q_3.py
+++ q_3.py
@@ -1,1 +1,24 @@
+'''
+3. Write a function to validate if the provided two strings are anagrams or not. If the two strings are anagrams, then return ‘yes’. Otherwise, return ‘no’.
+Input:
 
+Input 1: 1st string
+Input 2: 2nd string
+
+Output:
+(If they are anagrams, the function will return ‘yes’. Otherwise, it will return ‘no’.)
+
+Example
+
+Input 1: Listen
+Input 2: Silent
+
+Output:
+Yes
+
+Explanation
+
+Listen and Silent are anagrams (an anagram is a word formed by rearranging the letters of the other word).
+'''
+
+#Solution
